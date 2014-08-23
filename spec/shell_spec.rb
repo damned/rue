7d25c7944e1f916313f06cc2528c73378100b161
danm <writetodan@yahoo.com>
2014-08-23 15:08:01 +0100

    @damned improve specs - remove more noise

--- spec/shell_spec.rb
+++ spec/shell_spec.rb
@@ -7,6 +7,10 @@ require_relative '../lib/rue'
 include Rue
 include Test::RueDoubles
 
+def project_root_dir
+  Pathname.new(__FILE__).parent.to_s
+end
+
 describe Shell do
 
   let(:terminal) { terminal_double }
@@ -20,20 +24,13 @@ describe Shell do
   describe 'basic commands' do
     describe 'echo' do
       it 'should echo string input' do
-        terminal.simulate 'echo "bob"'
-        shell.run
-        expect(terminal.output).to comprise 'echo "bob"',
-                                            'bob'
+        expect(output_from 'echo "bob"').to comprise 'echo "bob"', 'bob'
       end
     end
     describe 'pwd' do
       xit 'should show current directory' do
-        terminal.simulate 'pwd'
-
-        shell.run
-
-        expect(terminal.output).to comprise 'pwd',
-                                            Pathname.new(__FILE__).parent.to_s
+        expect(output_from 'pwd').to comprise 'pwd',
+                                              project_root_dir
       end
     end
     describe 'ls' do
@@ -41,6 +38,11 @@ describe Shell do
     end
 
 
+    def output_from(commands)
+      terminal.simulate commands
+      shell.run
+      terminal.output
+    end
   end
 
 end
