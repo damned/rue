12f1deb155df9393a55dc2463480ad6c0550346d
danm <writetodan@yahoo.com>
2014-08-23 10:29:36 +0100

    @damned some basic command line handling

---
+++ lib/commands/ls.rb
@@ -0,0 +1,12 @@
+module Rue
+  module Command
+    class Ls
+      module Dsl
+        def ls(spec)
+          
+        end
+      end
+    end
+  end
+end
+
---
+++ rue.rb
@@ -0,0 +1,67 @@
+require 'io/console'
+
+class Terminal
+  def get_char
+    STDIN.getch
+  end
+  def print(s)
+    STDOUT.print s
+  end
+end
+
+
+class LineDiscipline
+  def initialize
+    @line = ''
+  end
+  def complete?
+    @complete || false
+  end
+  def add(c)
+    if complete? || c == "\r"
+      @complete = true
+      return "\r\n"
+    end
+    @line +=c
+    c
+  end
+  def to_s
+    @line
+  end
+end
+
+class ExecutionContext
+  def method_missing(method, *params)
+    puts "running (honest): #{method} #{params}"
+  end
+  def exit
+    raise 'Had enough, exiting...'
+  end
+end
+
+class Executor
+  def initialize
+    @context = ExecutionContext.new
+  end
+  def exec(line)
+    @context.instance_eval line.to_s
+  end
+end
+
+begin
+  executor = Executor.new
+  term = Terminal.new
+
+  while true do
+
+    line = LineDiscipline.new
+
+    until line.complete?
+      term.print line.add(term.get_char)
+    end
+    
+    executor.exec line
+  end
+rescue => e
+  puts "you're just mucking around now, rue has had enough: #{e}"
+end
